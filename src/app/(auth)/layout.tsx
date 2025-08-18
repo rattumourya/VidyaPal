3fc803684549edf203e57af93a86af81b02b1bd2
VidyaPal <vidyapaledu@gmail.com>
2025-08-18 13:48:40 +0000

    Header on login screen and signup screen is not visible

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,13 @@
+import { Header } from "@/components/shared/header";
 import type { ReactNode } from "react";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex min-h-screen w-full items-center justify-center bg-gradient-to-br from-background to-blue-100">
-      {children}
+    <div className="flex min-h-screen w-full flex-col">
+      <Header />
+      <main className="flex flex-1 items-center justify-center bg-gradient-to-br from-background to-blue-100">
+        {children}
+      </main>
     </div>
   );
 }
